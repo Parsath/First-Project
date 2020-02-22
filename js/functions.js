ab3b38ddde9237f9f458439f34aaa6197551bf3a
Parsath <bilel.taktak@esprit.tn>
2020-02-22 18:39:15 +0100

    Update

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,23 @@
 $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
-
-  console.log(wScroll);
-  
   
   $('.row1').css({
-    'transform' : 'translate(0px, '+ wScroll /10 +'%)'
+    'transform' : 'translate(0px, '+ wScroll /10.3 +'%)'
   });
 
   $('.logo').css({
     'transform' : 'translate(0px, -'+ wScroll /4 +'%)'
   });
-  
+
+  if(wScroll > 200){
+    console.log("hi");
+    $('.figures').addClass('is-dissapearing');
+  }
+
+  if(wScroll < $('header').offset().top + 1){
+    console.log("hi");
+    $('.figures').removeClass('is-dissapearing');
+  }
 
 });
