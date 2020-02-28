4b80e8915de6e4bd232db78758dc4720926f86b7
Parsath <bilel.taktak@esprit.tn>
2020-02-28 20:59:30 +0100

    Updaaateeuuu

--- js/functions.js
+++ js/functions.js
@@ -5,20 +5,53 @@ $(window).scroll(function(){
   $('.row1').css({
     'transform' : 'translate(0px, '+ wScroll /10.3 +'%)'
   });  
-  
 
   $('.logo').css({
     'transform' : 'translate(0px, -'+ wScroll /6 +'%)'
   });
 
-  if(wScroll > 200){
-    console.log("hi");
+  if(wScroll > 100){
+
     $('.figures').addClass('is-dissapearing');
+
+    $('.nav__links li').each(function(i){
+
+      setTimeout(function(){
+        $('.nav__links li').eq(i).addClass('is-unlanding');
+      }, 150 * (i+1));
+
+    });
+
+    $('button').addClass('got-out');
+
   }
 
   if(wScroll < $('header').offset().top + 1){
-    console.log("hi");
     $('.figures').removeClass('is-dissapearing');
+
+    $('.nav__links li').each(function(i){
+      setTimeout(function(){
+        $('.nav__links li').eq(i).removeClass('is-unlanding');
+      }, 150 * (i+1));
+    });
+
+    $('button').removeClass('got-out');
+
   }
+
+});
+
+
+$(window).load(function(){
+
+  $('button').addClass('is-showing');
+
+  $('.figures').addClass('is-appearing');
+
+  $('.nav__links li').each(function(i){
+    setTimeout(function(){
+      $('.nav__links li').eq(i).addClass('is-landing');
+    }, 150 * (i+1));
+  });
 
 });
